98a23a95a5e83dea5842c3fef16b70da147eb0f9
tinachats <marshallchaterera@gmail.com>
2022-07-09 10:55:49 +0200

    Match listing

--- assets/js/mwos.js
+++ assets/js/mwos.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Enable Popovers
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    });
+
+    // Enable Tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+
     /*** Lazy load images ***/
     let lazyLoadImages;
     if ('IntersectionObserver' in window) {
