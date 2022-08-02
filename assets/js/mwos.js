437b10dc0a847c6ac7298a800c5f3d2650b0fb30
tinachats <marshallchaterera@gmail.com>
2022-08-02 18:32:57 +0200

    Bookmark matches UI

--- assets/js/mwos.js
+++ assets/js/mwos.js
@@ -192,6 +192,36 @@ function animatedCounter() {
     });
 }
 
+// Countdown Timers
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var currTime = now.getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - currTime;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
 document.addEventListener('DOMContentLoaded', () => {
     // Initialize AOS
     AOS.init();
