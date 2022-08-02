6ad9557d15e6a4ac9e930b2431664e2a829fb51b
tinachats <marshallchaterera@gmail.com>
2022-08-02 22:07:46 +0200

    App timers

--- assets/js/mwos.js
+++ assets/js/mwos.js
@@ -192,13 +192,16 @@ function animatedCounter() {
     });
 }
 
-// Countdown Timers
+/*** Countdown Timer ***/
+
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate = new Date('Aug 14, 2022 15:37:25').getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+// var x = setInterval(countDownTimer, 1000);
 
+// Count down timer
+function countDownTimer(el, countDownDate) {
     // Get today's date and time
     var currTime = now.getTime();
 
@@ -211,17 +214,65 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
-        minutes + "m " + seconds + "s ";
+    // Display the result in the element
+    document.querySelector(el).innerHTML = `${days}:${hours}:${minutes}:${minutes}:${seconds}`;
 
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById(el).innerHTML = 'ENDED';
     }
-}, 1000);
+}
 
+/*** /. Countdown Timer ***/
+
+/*** Count up timer ***/
+
+//Get the starting time (right now) in seconds
+var startTime = Math.floor(Date.now() / 1000);
+
+// Store it if I want to restart the timer on the next page
+localStorage.setItem('startTime', startTime);
+
+function startTimeCounter() {
+    // get the time now
+    var now = Math.floor(Date.now() / 1000);
+
+    // diff in seconds between now and start
+    var diff = now - startTime;
+
+    // get minutes value (quotient of diff)
+    var m = Math.floor(diff / 60);
+
+    // get seconds value (remainder of diff)
+    var s = Math.floor(diff % 60);
+
+    // add a leading zero if it's single digit
+    m = checkTime(m);
+    s = checkTime(s);
+
+    // update the element where the timer will 
+    const matchTimers = document.querySelectorAll('.match-timer');
+
+    matchTimers.forEach((matchTimer) => {
+        matchTimer.innerHTML = m + ":" + s;
+    });
+
+    // set a timeout to update the timer
+    var t = setTimeout(startTimeCounter, 500);
+}
+
+function checkTime(i) {
+    // add zero in front of numbers < 10
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
+
+startTimeCounter();
+
+/*** /. Count up timer ***/
 document.addEventListener('DOMContentLoaded', () => {
     // Initialize AOS
     AOS.init();
